b403a08942c65abac619363c8b1753cdc5022a0e
Andrew <andrew@aguest.me>
2020-12-12 21:18:30 -0600

    Update and rename api.py to main.py

--- {{cookiecutter.app_slug}}/app/main.py
+++ {{cookiecutter.app_slug}}/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from routes import jwt_routes, user_routes
 
+
 # to get a string like this run:
 # openssl rand -hex 32
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
